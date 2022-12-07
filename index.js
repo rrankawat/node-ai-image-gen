4564d52dcc878f0470d68870b391b59d7f2e6e15
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2022-12-07 23:18:25 +0530

    Image Gen - UI

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config()
+const path = require('path')
 
 const port = process.env.PORT || 5000
 const app = express()
@@ -7,6 +8,9 @@ const app = express()
 // Enable body parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/openai', require('./routes/openaiRoutes'))
 
